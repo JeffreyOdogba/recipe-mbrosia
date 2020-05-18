c2fb5231671f1130c1969d760c51f332722d5c40
Jeffrey Odogba <odogbajeffrey@gmail.com>
2020-05-18 11:49:22 -0400

    it works

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -5,12 +5,15 @@ const welcome_1 = require("../assets/welcome_1.jpg");
 function WelcomeScreen(props) {
   return (
     <ImageBackground source={welcome_1} style={styles.background}>
-      <Text style={styles.header}>
-        Welcome to
-        <b>
-          <i> Mbrosia Recipe</i>
-        </b>
-      </Text>
+      <View>
+        <Text style={styles.header}>
+          <Text>Welcome to </Text>
+          <Text style={{ fontWeight: "bold", fontStyle: "italic" }}>
+            Mbrosia Recipe
+          </Text>
+        </Text>
+      </View>
+
       <View style={styles.loginBtn}>
         <Button title="Login" />
       </View>
